0d7c2e044dced9d78da1172e7ab6eb7fb2848bcf
helgorn <rusiecki.michal@gmail.com>
2018-10-21 12:55:01 +0200

    Cos dziala

---
+++ src/com/sda/kolekcje/CustomList.java
@@ -0,0 +1,62 @@
+package com.sda.kolekcje;
+
+import java.util.Arrays;
+
+//implementacja wlasnej arraylist
+public class CustomList<E> {
+
+    private int size = 0;
+    private final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public CustomList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public void add(E e) {
+        if(size == elements.length){
+            resize();
+        }
+        elements[size++] = e;
+    }
+
+    public void resize(){
+        int newSize = elements.length *2;
+        Object tab[] = new Object[newSize];
+        for (int i=0; i<elements.length; i++){
+            tab[i]=elements[i];
+        }
+        elements = tab;
+
+        //inne rozwiazanie
+        //elements = Arrays.copyOf(elements,newSize);
+    }
+
+    public E get(int i){
+        if(i>=size || i<0){
+            throw new IndexOutOfBoundsException("Nie ma takiego elementu");
+        }
+        return (E) elements[i];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Object[] getElements() {
+        return elements;
+    }
+
+    public void remove(int index){
+        Object tmp[]=new Object[size];
+        for (int i=0; i<elements.length; i++){
+            if(i<index){
+                tmp[i]=elements[i];
+            }
+            if(i>index){
+                tmp[i-1]=elements[i];
+            }
+        }
+        elements=tmp;
+    }
+}
--- src/com/sda/kolekcje/Main.java
+++ src/com/sda/kolekcje/Main.java
@@ -3,6 +3,35 @@ package com.sda.kolekcje;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        CustomList<String> lista = new CustomList<>();
+        lista.add("Jan");
+        lista.add("Michal");
+        lista.add("Jan");
+        lista.add("Michal");
+        lista.add("Rafal12");
+        lista.add("Michal");
+        lista.add("Rafal");
+        lista.add("Michal");
+        lista.add("Rafal");
+        lista.add("Michal");
+        lista.add("Rafal");
+        lista.add("Michal");
+        lista.add("Rafal");
+
+
+        for (int i = 0; i < lista.getSize(); i++) {
+            System.out.println(lista.getElements()[i]);
+        }
+        System.out.println("Rozmiar: " + lista.getSize());
+
+        for(int i=0; i < lista.getSize();i++){
+            System.out.println(lista.get(i));
+        }
+
+        lista.remove(4);
+        for(int i=0; i < lista.getSize();i++){
+            System.out.println(lista.get(i));
+        }
     }
 }
